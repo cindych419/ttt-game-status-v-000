8ade91f0ea24737a2760bbe515261a863e91af63
cindych419@gmail.com <cindych419@gmail.com>
2018-04-24 22:59:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,16 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6],
+  [0,4,8]
+]# Define your WIN_COMBINATIONS constant
+if board[index[0]] == "X" && board[index[1]] == "X" && board[index[2]] == "X"
+  board[index[0]],  board[index[1]],  board[index[2]]
+end
