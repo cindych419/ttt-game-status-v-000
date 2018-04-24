01c1d658c2bb9fdb0723ca92c0b485a24a1b4318
cindych419@gmail.com <cindych419@gmail.com>
2018-04-24 23:57:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,3 +18,7 @@ def won?(board)
     board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && position_taken?(board, combo[0])
     end
 end
+
+def full?(board)
+  position_taken(board, index)
+end
